5a72eab67ef6e59876b493c93a509312dfb1c863
mohammad noor <g.s.dadanoor@gmail.com>
2024-07-29 18:46:43 +0530

    allowing the localhost

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 console.log("enterd app.js")
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+
+//allowing the localhost  for development
+
+app.use(cors({
+    origin: 'http://localhost:3000' // Allow requests from your Next.js app's origin
+  }));
 
 
 //user routes
